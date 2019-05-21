bd226684ff782451fce0779ed47c003c5ba695d0
weazy <weazy.exe@gmail.com>
2019-05-21 18:36:29 +0300

    upd settings, more animations, dark theme hint :)

---
+++ app/src/main/java/exe/weazy/extendenglish/UserActivity.kt
@@ -0,0 +1,12 @@
+package exe.weazy.extendenglish
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class UserActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_user)
+    }
+}
--- app/src/main/java/exe/weazy/extendenglish/ui/fragment/AccountFragment.kt
+++ app/src/main/java/exe/weazy/extendenglish/ui/fragment/AccountFragment.kt
@@ -1,5 +1,8 @@
 package exe.weazy.extendenglish.ui.fragment
 
+import android.app.Activity
+import android.app.ActivityOptions
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -7,6 +10,7 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.GridLayoutManager
 import exe.weazy.extendenglish.R
+import exe.weazy.extendenglish.UserActivity
 import exe.weazy.extendenglish.adapter.CategoriesRecyclerViewAdapter
 import exe.weazy.extendenglish.entity.Category
 import kotlinx.android.synthetic.main.fragment_account.*
@@ -42,5 +46,11 @@ class AccountFragment : Fragment() {
 
         username_text.text = username
         level_text.text = level
+
+        account_card.setOnClickListener {
+            val intent = Intent(context, UserActivity::class.java)
+            val options = ActivityOptions.makeSceneTransitionAnimation(activity as Activity, avatar_imageView, "avatar_image")
+            startActivity(intent, options.toBundle())
+        }
     }
 }
